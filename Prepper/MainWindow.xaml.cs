36196b5a3fe347c9e147df7dcd9f0e040f980322
Darrell Swiney <drswin01@live.com>
2023-07-05 23:58:20 -0400

    Fixing maximize and window move

--- Prepper/MainWindow.xaml.cs
+++ Prepper/MainWindow.xaml.cs
@@ -7,7 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace Prepper
 {
@@ -20,6 +23,7 @@ namespace Prepper
         private const string TheTvDbPinKeyEnvironmentVariableName = "TvDbPin";
         private const string TheTvDbSubscribeUrl = "https://thetvdb.com/subscribe";
         private ReferenceSelectionControl selectionControl;
+        private bool mRestoreIfMove = false;
 
         public MainViewModel ViewModel { get; set; }
         public MainWindow()
@@ -109,12 +113,7 @@ namespace Prepper
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            Close();
-        }
-
-        private void ControlBox_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            DragMove();
+            Application.Current.Shutdown();
         }
 
         private void SelectedDirectoryTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -152,7 +151,7 @@ namespace Prepper
                     {
                         Process.Start(new ProcessStartInfo(TheTvDbSubscribeUrl) { UseShellExecute = true });
                     }
-                    Close();
+                    Application.Current.Shutdown();
                     return;
                 }
             }
@@ -173,7 +172,7 @@ namespace Prepper
             if (tvDb == null)
             {
                 MessageBox.Show("Unable to get TvDbProvider, closing application");
-                Close();
+                Application.Current.Shutdown();
                 return;
             }
             this.Opacity = 1;
@@ -217,13 +216,196 @@ namespace Prepper
 
         private void MaximizeButton_Click(object sender, RoutedEventArgs e)
         {
-            if (this.WindowState.Equals(WindowState.Normal)) 
+            SwitchWindowState();
+        }
+
+        void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            IntPtr mWindowHandle = (new WindowInteropHelper(this)).Handle;
+            HwndSource.FromHwnd(mWindowHandle).AddHook(new HwndSourceHook(WindowProc));
+        }
+
+        private static System.IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
             {
-                this.WindowState = WindowState.Maximized;
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    break;
             }
-            else 
+
+            return IntPtr.Zero;
+        }
+
+
+        private static void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
+        {
+            POINT lMousePosition;
+            GetCursorPos(out lMousePosition);
+
+            IntPtr lPrimaryScreen = MonitorFromPoint(new POINT(0, 0), MonitorOptions.MONITOR_DEFAULTTOPRIMARY);
+            MONITORINFO lPrimaryScreenInfo = new MONITORINFO();
+            if (GetMonitorInfo(lPrimaryScreen, lPrimaryScreenInfo) == false)
             {
-                this.WindowState = WindowState.Normal;
+                return;
+            }
+
+            IntPtr lCurrentScreen = MonitorFromPoint(lMousePosition, MonitorOptions.MONITOR_DEFAULTTONEAREST);
+
+            MINMAXINFO lMmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO))!;
+
+            if (lPrimaryScreen.Equals(lCurrentScreen) == true)
+            {
+                lMmi.ptMaxPosition.X = lPrimaryScreenInfo.rcWork.Left;
+                lMmi.ptMaxPosition.Y = lPrimaryScreenInfo.rcWork.Top;
+                lMmi.ptMaxSize.X = lPrimaryScreenInfo.rcWork.Right - lPrimaryScreenInfo.rcWork.Left;
+                lMmi.ptMaxSize.Y = lPrimaryScreenInfo.rcWork.Bottom - lPrimaryScreenInfo.rcWork.Top;
+            }
+            else
+            {
+                lMmi.ptMaxPosition.X = lPrimaryScreenInfo.rcMonitor.Left;
+                lMmi.ptMaxPosition.Y = lPrimaryScreenInfo.rcMonitor.Top;
+                lMmi.ptMaxSize.X = lPrimaryScreenInfo.rcMonitor.Right - lPrimaryScreenInfo.rcMonitor.Left;
+                lMmi.ptMaxSize.Y = lPrimaryScreenInfo.rcMonitor.Bottom - lPrimaryScreenInfo.rcMonitor.Top;
+            }
+
+            Marshal.StructureToPtr(lMmi, lParam, true);
+        }
+
+        private void SwitchWindowState()
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+                    {
+                        WindowState = WindowState.Maximized;
+                        break;
+                    }
+                case WindowState.Maximized:
+                    {
+                        WindowState = WindowState.Normal;
+                        break;
+                    }
+            }
+        }
+
+        private void rctHeader_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if ((ResizeMode == ResizeMode.CanResize) || (ResizeMode == ResizeMode.CanResizeWithGrip))
+                {
+                    SwitchWindowState();
+                }
+
+                return;
+            }
+
+            else if (WindowState == WindowState.Maximized)
+            {
+                mRestoreIfMove = true;
+                return;
+            }
+
+            DragMove();
+        }
+
+
+        private void rctHeader_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            mRestoreIfMove = false;
+        }
+
+        private void rctHeader_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (mRestoreIfMove)
+            {
+                mRestoreIfMove = false;
+
+                double percentHorizontal = e.GetPosition(this).X / ActualWidth;
+                double targetHorizontal = RestoreBounds.Width * percentHorizontal;
+
+                double percentVertical = e.GetPosition(this).Y / ActualHeight;
+                double targetVertical = RestoreBounds.Height * percentVertical;
+
+                WindowState = WindowState.Normal;
+
+                POINT lMousePosition;
+                GetCursorPos(out lMousePosition);
+
+                //Left = lMousePosition.X - targetHorizontal;
+                //Top = lMousePosition.Y - targetVertical;
+
+                DragMove();
+            }
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr MonitorFromPoint(POINT pt, MonitorOptions dwFlags);
+
+        enum MonitorOptions : uint
+        {
+            MONITOR_DEFAULTTONULL = 0x00000000,
+            MONITOR_DEFAULTTOPRIMARY = 0x00000001,
+            MONITOR_DEFAULTTONEAREST = 0x00000002
+        }
+
+
+        [DllImport("user32.dll")]
+        static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+            public RECT rcMonitor = new RECT();
+            public RECT rcWork = new RECT();
+            public int dwFlags = 0;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left, Top, Right, Bottom;
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.Left = left;
+                this.Top = top;
+                this.Right = right;
+                this.Bottom = bottom;
             }
         }
     }
